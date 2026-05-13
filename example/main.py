537ddce0fa53b36c226fd8f87d1e38df434c42a6
dgumenyuK <gumenyuk98@gmail.com>
2026-05-13 06:47:38 -0400

    fix: updating example

--- example/main.py
+++ example/main.py
@@ -11,21 +11,21 @@ if __name__ == "__main__":
     vehicle_detector = VehicleDetector(config_path)
     labeling_pipeline: LabelingPipeline = vehicle_detector.labeling_pipeline
     # labeling_pipeline.visualize_predictions()
-    # labeling_pipeline.run()
-    # labeling_pipeline.remove_items_by_id(
-    #     item_id_list=[
-    #         "train_C_640_360_24fps_000004",
-    #         "train_C_640_360_24fps_000005",
-    #         "train_C_640_360_24fps_000006",
-    #         "train_C_640_360_24fps_000007",
-    #         "train_C_640_360_24fps_000008",
-    #         "train_C_640_360_24fps_000009",
-    #         "train_C_640_360_24fps_000010",
-    #         "train_C_640_360_24fps_000012",
-    #         "train_C_640_360_24fps_000013",
-    #         "train_C_640_360_24fps_000015",
-    #     ]
-    # )
+    labeling_pipeline.run()
+    labeling_pipeline.remove_items_by_id(
+        item_id_list=[
+            "train_C_640_360_24fps_000004",
+            "train_C_640_360_24fps_000005",
+            "train_C_640_360_24fps_000006",
+            "train_C_640_360_24fps_000007",
+            "train_C_640_360_24fps_000008",
+            "train_C_640_360_24fps_000009",
+            "train_C_640_360_24fps_000010",
+            "train_C_640_360_24fps_000012",
+            "train_C_640_360_24fps_000013",
+            "train_C_640_360_24fps_000015",
+        ]
+    )
     # training_pipeline: TrainingPipeline = vehicle_detector.training_pipeline
     # training_pipeline.run()
     evaluation_pipeline: EvaluationPipeline = vehicle_detector.evaluation_pipeline
